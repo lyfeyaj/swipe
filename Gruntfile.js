b80bb5a1ffc37d2f0cf1f10ead03c3d836a4e652
lyfeyaj <lyfeyaj@gmail.com>
2016-11-03 13:55:22 +0800

    update doc

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function( grunt ) {
       },
       demo: {
         files: {
-          './vendor/style.css': './sass/style.scss'
+          './dist/style.css': './sass/style.scss'
         }
       }
     },
