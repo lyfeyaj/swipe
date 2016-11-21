4aef10abe1ab38424c01fd336fd59205804710dc
lyfeyaj <lyfeyaj@gmail.com>
2016-11-21 19:51:02 +0800

    code style

--- swipe.js
+++ swipe.js
@@ -31,8 +31,8 @@
   // on the server, or `this` in some virtual machines. We use `self`
   // instead of `window` for `WebWorker` support.
   var root = typeof self == 'object' && self.self === self && self ||
-            typeof global == 'object' && global.global === global && global ||
-            this;
+             typeof global == 'object' && global.global === global && global ||
+             this;
 
   var _document = root.document || this.document;
 
