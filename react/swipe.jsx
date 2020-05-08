193f4fd07933e311a1edcbea7a6cb86ea911e49d
lyfeyaj <lyfeyaj@gmail.com>
2020-05-08 20:33:18 +0800

    bump up version

--- react/swipe.jsx
+++ react/swipe.jsx
@@ -1,5 +1,5 @@
 /*!
- * React Swipe 2.2.16
+ * React Swipe 2.2.17
  *
  * Felix Liu
  * Copyright 2016 - 2018, MIT License
--- swipe.js
+++ swipe.js
@@ -1,5 +1,5 @@
 /*!
- * Swipe 2.2.16
+ * Swipe 2.2.17
  *
  * Brad Birdsall
  * Copyright 2013, MIT License
